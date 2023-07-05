d8f7ebdc9dbbee1729ed9545d857a780eb44c244
dMuguti <omiahderek56@gmail.com>
2023-07-05 15:19:48 +0300

    postcontrollers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,12 @@
 class SessionsController < ApplicationController
-    def create
-        user = User.find_by(email: params[:email])
-        if user && user.authenticate(params[:password])
-          token = JwtToken.encode(user_id: user.id)
-          cookies.signed[:jwt] = { value: token, httponly: true }
-          render json: { user: user, token: token }
-        else
-          render json: { errors: ['Invalid email or password'] }, status: :unauthorized
-        end
+  def create
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      token = JWT.encode({ user_id: user.id }, ENV['SECRET_KEY'], 'HS256')
+      render json: { token: token, user: user }
+    else
+      render json: { error: "Invalid username or password" }, status: :unauthorized
     end
-    
-    def destroy
-        cookies.delete(:jwt)
-        head :no_content
-    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,28 @@
 class UsersController < ApplicationController
-
     def create
-        user = User.create(user_params)
-        if user.valid?
-            session[:user_id] = user.id
-            render json: user, status: :created
-        else
-            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-        end
-    end
-
-    def index
-        users = User.all
-        render json: users, each_serializer: UserSerializer
+      user = User.new(user_params)
+      if user.save
+        token = JWT.encode({ user_id: user.id }, ENV['SECRET_KEY'], 'HS256') # use your actual secret key
+        render json: { user: user, token: token }, status: :created
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
     end
     
+    def index
+      users = User.all
+      render json: users, each_serializer: UserSerializer
+    end
+  
     def show
-        user = User.find(params[:id])
-        render json: user, serializer: UserSerializer, include: ['posts.comments', 'posts.likes']
+      user = User.find(params[:id])
+      render json: user, serializer: UserSerializer, include: ['posts.comments', 'posts.likes']
     end
-
+  
     private
-
+  
     def user_params
-        params.permit(:email, :username, :password, :password_confirmation)
+      params.require(:user).permit(:email, :username, :password, :password_confirmation)
     end
-
-end
+  end
+  
